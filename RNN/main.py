55f3408770ad6ebc1737ddb7f668b317f9bb0e7b
LeNotaCoder <yadyneshsonale@gmail.com>
2025-07-17 12:34:16 +0530

    main and functions update

--- RNN/main.py
+++ RNN/main.py
@@ -44,21 +44,6 @@ criterion = nn.NLLLoss()
 learning_rate = 0.005
 optimizer = SGD(rnn.parameters(), lr=learning_rate)
 
-def train(word_tensor, label_tensor):
-
-    hidden = rnn.init_hidden()
-
-    for i in range(word_tensor.size()[0]):
-        output, hidden = rnn(word_tensor[i], hidden)
-    
-    loss = criterion(output, label_tensor)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    return output, loss.item()
-
 current_loss = 0.0
 all_losses = []
 
