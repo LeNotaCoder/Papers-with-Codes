b42d6032d96d34c7407fe2e741291c4fe8076bd6
LeNotaCoder <yadyneshsonale@gmail.com>
2025-07-17 12:37:47 +0530

    removed functions

--- RNN/main.py
+++ RNN/main.py
@@ -47,6 +47,24 @@ optimizer = SGD(rnn.parameters(), lr=learning_rate)
 current_loss = 0.0
 all_losses = []
 
+def train(word_tensor, label_tensor):
+
+    hidden = rnn.init_hidden()
+
+    for i in range(word_tensor.size()[0]):
+        output, hidden = rnn(word_tensor[i], hidden)
+    
+    loss = criterion(output, label_tensor)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return output, loss.item()
+
+def get_index(output):
+    return torch.argmax(output)
+
 for epoch in range(NUM_EPOCHS):
     for words, labels in train_loader:
         for j in range(len(labels)):
